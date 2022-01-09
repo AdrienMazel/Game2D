48e1e8e9cb4a216b6bcfe7440ebf52a8bf7b8aeb
Adrien <mazelcraft.officiel@gmail.com>
2022-01-09 12:14:10 +0100

    v.final

---
+++ Project_Game/Assets/LoadAndSaveData.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class LoadAndSaveData : MonoBehaviour
+{
+
+    public static LoadAndSaveData instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de LoadAndSaveData dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    void Start()
+    {
+        Inventory.instance.coinsCount = PlayerPrefs.GetInt("coinsCount", 0);
+        Inventory.instance.UpdateTextUI();
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("coinsCount", Inventory.instance.coinsCount);
+        PlayerPrefs.SetInt("levelReached", CurrentSceneManager.instance.levelToUnlock);
+
+        if (CurrentSceneManager.instance.levelToUnlock > PlayerPrefs.GetInt("levelReached", 1))
+        {
+            PlayerPrefs.SetInt("levelReached", CurrentSceneManager.instance.levelToUnlock);
+        }
+    }
+}
